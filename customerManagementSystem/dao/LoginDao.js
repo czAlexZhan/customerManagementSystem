6fa0102605beec0e9dc70f6bc9230b98e3e18d45
czAlexZhan <1006452606@qq.com>
2017-07-20 18:22:29 +0800

    #version 1.0.1 登录与注册

--- customerManagementSystem/dao/LoginDao.js
+++ customerManagementSystem/dao/LoginDao.js
@@ -1,9 +1,20 @@
 var sqlPool = require('../service/MysqlService');
+var logger = require('../service/LogService');
 
 module.exports = {
     getUserInfoByUserName:function (name) {
         sqlPool.getConnection(function (err, connection) {
-
+        	var sql = "select * from sys_user where name='"+name+"'";
+			connection.query(sql,function(err,result){
+				if(err){
+					logger.info("执行方法 getUserInfoByUserName 出现错误："+err);
+				}else{
+					console.log(result);
+					return result;
+				}
+			
+			});
+			connection.release(); 
         });
     }
 };
